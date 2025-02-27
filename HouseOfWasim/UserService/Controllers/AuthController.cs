d1c0d5ff12c3557630a502af1400cd713331c164
Wasim4Malik <66451575+Wasim4Malik@users.noreply.github.com>
2025-02-27 14:16:25 -0500

    initial step up
    set up api file and report controller with api endpoints using inbuild methods to handle user register and login

---
+++ HouseOfWasim/UserService/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Models;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+            private readonly UserManager<User> _userManager;
+            private readonly SignInManager<User> _signInManager;
+
+            public AuthController(UserManager<User> userManager, SignInManager<User> signInManager)
+            {
+                _userManager = userManager;
+                _signInManager = signInManager;
+            }
+
+            [HttpPost("register")]
+            public async Task<IActionResult> Register(UserRegistrationModel model)
+            {
+                var user = new User { UserName = model.Username, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded) return Ok();
+                return BadRequest(result.Errors);
+            }
+
+            [HttpPost("login")]
+            public async Task<IActionResult> Login(UserLoginModel model)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, false, false);
+                if (result.Succeeded) return Ok();
+                return Unauthorized();
+            }
+        
+    }
+}
---
+++ HouseOfWasim/UserService/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserService.Models;
+
+namespace UserService.Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
